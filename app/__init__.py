1c47d75b85a3fc8618c12c04400a4ab86039ec26
Artem <111882632+ArtemZahreba@users.noreply.github.com>
2025-05-29 13:53:23 +0300

    Improvements and bug fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from app.routes import bp
 from app.config import Config
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.register_blueprint(bp)
+
+    CORS(app)
     return app
